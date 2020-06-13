04aa8fd6a2163951c48e7633a090025727bde044
edo1z <endo.yuta@logicky.com>
2020-06-13 19:35:34 +0900

    繰り返します

--- src/index.js
+++ src/index.js
@@ -4,28 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const Hello = (props) => <p>Hello!<br />{props.msg}</p>
-const GoodNight = (props) => <p>Good Night!<br />{props.msg}</p>
-const NightDesu = (props) => !props.isNight ? null : <p>夜ですね</p>;
-
-const Greeting = props => {
-  const msg = 'My name is tanaka.';
-  return (
-    <div>
-      {props.isNight && <GoodNight msg={msg} />}
-      {props.isNight
-        ? <GoodNight msg={msg} />
-        : <Hello msg={msg} />
-      }
-      <NightDesu isNight={props.isNight} />
-    </div>
-  )
+const Numbers = props => {
+  const listItems = props.numbers.map(n => {
+    return <li key={n.toString()}>{n * 3}</li>
+  });
+  return <ul>{listItems}</ul>
 };
 
 const root = document.getElementById('root');
+const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
   <React.StrictMode>
-    <Greeting isNight={true} />
+    <Numbers numbers={numbers} />
   </React.StrictMode>,
   root
 );
