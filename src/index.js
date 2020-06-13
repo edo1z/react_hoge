41dcee317b378e25065426e9c2701f618a00d8f1
edo1z <endo.yuta@logicky.com>
2020-06-13 19:11:31 +0900

    条件をつけます

--- src/index.js
+++ src/index.js
@@ -4,62 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: props.id,
-      active: true,
-      date: new Date()
-    };
-    // this.handleClick = this.handleClick.bind(this);
-  }
+const Hello = (props) => <p>Hello!<br />{props.msg}</p>
+const GoodNight = (props) => <p>Good Night!<br />{props.msg}</p>
+const NightDesu = (props) => !props.isNight ? null : <p>夜ですね</p>;
 
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    if (this.state.active) {
-      this.setState({
-        date: new Date()
-      })
-    }
-  }
-
-  handleClick(a, b, id, e) {
-    e.preventDefault();
-    console.log(id + 'がクリックされました');
-    console.log(this.state.id);
-    this.setState(state => ({active: !state.active}));
-  }
-
-  render() {
-    return (
-      <div>
-        <p>Clock - {this.state.id}</p>
-        {this.state.date.toLocaleTimeString()}
-        <a
-          href="https://google.com"
-          onClick={this.handleClick.bind(this, 10, 20, this.state.id)}
-        >
-          Delete
-        </a>
-      </div>
-    );
-  }
-}
+const Greeting = props => {
+  const msg = 'My name is tanaka.';
+  return (
+    <div>
+      {props.isNight && <GoodNight msg={msg} />}
+      {props.isNight
+        ? <GoodNight msg={msg} />
+        : <Hello msg={msg} />
+      }
+      <NightDesu isNight={props.isNight} />
+    </div>
+  )
+};
 
 const root = document.getElementById('root');
 ReactDOM.render(
   <React.StrictMode>
-    <Clock id="1" />
-    <Clock id="2" />
-    <Clock id="3" />
+    <Greeting isNight={true} />
   </React.StrictMode>,
   root
 );
