ee47657654210de664abc5538ec6bb71b15d3f30
edo1z <endo.yuta@logicky.com>
2020-07-10 10:12:00 +0900

    use hook

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Hoge = props => (
+const Form = props => {
+  return (
+    <input
+      type="text"
+      value={props.txt}
+      onChange={props.onChange}
+    />
+  )
+};
+
+const Text = props => (
   <p>{props.txt}</p>
 );
 
+const App = () => {
+  const [value, setValue] = useState('');
+  const onChangeHandler = e => setValue(e.target.value);
+  return (
+    <div>
+      <Form txt={value} onChange={onChangeHandler} />
+      <Text txt={value} />
+    </div>
+  );
+};
+
 ReactDOM.render(
-  <Hoge txt="HOGE"/>,
+  <App />,
   document.getElementById('root')
 );
